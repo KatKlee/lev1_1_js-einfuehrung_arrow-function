430bde4a8498df180734d855355eaa49b90acfbf
KatKlee <katklee2022@gmail.com>
2022-07-28 15:15:58 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/* Normale Funktion */
+function intro() {
+    console.log('Hello World')
+}
+
+intro()
+
+
+/* Arrow Funktion - lang */
+const arrow = () => {
+    console.log('Hello World')
+}
+
+arrow()
+
+
+/* Arrow Funktion - kurz */
+arrowkurz = () => console.log('Hello World')
+
+arrowkurz()
